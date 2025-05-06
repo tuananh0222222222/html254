466171a096d3f1817eb4d39defb155884974517c
kta <xaydagolds@gmail.com>
2025-05-06 19:35:10 +0700

    add

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggle = document.querySelector("[data-nav-toggler]");
+
+
+navToggle.addEventListener('click', () => {
+    navbar.classList.toggle("active")
+    navToggle.classList.toggle("active")
+
+})
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+    header.classList[window.scrollY > 100 ? "add" : "remove"]("active")
+
+})
