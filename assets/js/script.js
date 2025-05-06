0bef6e2569ef19a225ce90a91cba95f0223ec223
kta <xaydagolds@gmail.com>
2025-05-06 20:18:48 +0700

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,13 @@ const navToggle = document.querySelector("[data-nav-toggler]");
 navToggle.addEventListener('click', () => {
     navbar.classList.toggle("active")
     navToggle.classList.toggle("active")
+  
 
 })
 
 const header = document.querySelector("[data-header]");
 
 window.addEventListener("scroll", () => {
-    header.classList[window.scrollY > 100 ? "add" : "remove"]("active")
+    header.classList[window.scrollY > 50 ? "add" : "remove"]("active")
 
 })
